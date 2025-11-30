367c39b089c179de24cb49480d3cc6fb46f61764
ferabdev <fer.ab14p@gmail.com>
2025-11-30 13:20:27 -0600

    fix de migracion

---
+++ ComuniApi/Migrations/20251130192006_Fix.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComuniApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EstatusId",
+                table: "EdoCuentas",
+                type: "int",
+                nullable: false,
+                defaultValue: 2,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EstatusId",
+                table: "EdoCuentas",
+                type: "int",
+                nullable: false,
+                defaultValue: 3,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 2);
+        }
+    }
+}
